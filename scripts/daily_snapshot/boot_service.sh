0420e7d103199e2bd0db80ababbeeabf6eb91e67
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-02 16:15:18 +0000

    delete unused imports

--- scripts/daily_snapshot/boot_service.sh
+++ scripts/daily_snapshot/boot_service.sh
@@ -1,2 +1,32 @@
 
 #!/bin/bash
+
+# The .env file contains environment variables that we want access to.
+set -o allexport
+# Trust that the `.env` exists in the CWD during the script execution.
+# shellcheck disable=SC1091
+source .env
+set +o allexport
+
+error=0
+
+# Check if an environment variable is set. If it isn't, set error=1.
+check_env () {
+    A="                            ";
+    echo -n "${A:0:-${#1}} $1: "
+    if [[ -z "${!1}" ]]; then
+        echo "❌"
+        error=1
+    else
+        echo "✅"
+    fi
+}
+
+# Check that the environment variables in the .env file have been defined.
+check_env "AWS_ACCESS_KEY_ID"
+check_env "AWS_SECRET_ACCESS_KEY"
+check_env "SLACK_API_TOKEN"
+check_env "SLACK_NOTIF_CHANNEL"
+
+if [ "$error" -ne "0" ]; then
+    echo "Please set the required environment variables and try again."
