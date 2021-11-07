2a923313365b1cf78d80d003c08c5936259f96f8
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-07 15:43:26 +0000

    added api

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -12,4 +12,4 @@ require 'active_support/time'
 
 BASE_FOLDER = get_and_assert_env_variable 'BASE_FOLDER'
 SLACK_TOKEN = get_and_assert_env_variable 'SLACK_API_TOKEN'
-CHANNEL = 
+CHANNEL = get_and_assert_env_vari
