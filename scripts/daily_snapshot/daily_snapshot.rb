36f9d2a45154ab06f17eca580139e7d56744de03
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-08 07:13:29 +0000

    add contributors

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -12,4 +12,6 @@ require 'active_support/time'
 
 BASE_FOLDER = get_and_assert_env_variable 'BASE_FOLDER'
 SLACK_TOKEN = get_and_assert_env_variable 'SLACK_API_TOKEN'
-CHANNEL = get_and_assert_env_vari
+CHANNEL = get_and_assert_env_variable 'SLACK_NOTIF_CHANNEL'
+
+C
