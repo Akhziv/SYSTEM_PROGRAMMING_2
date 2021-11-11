379e2f2cfb295198d9941c6fbe4d9bfec2fc2525
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-11 15:59:15 +0000

    Update RK Max Flash.lua

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -26,4 +26,4 @@ SNAPSHOTS_DIR = File.join(BASE_FOLDER, 's3', CHAIN_NAME)
 loop do
   client = SlackClient.new CHANNEL, SLACK_TOKEN
 
-  # Find the snapshot with the most 
+  # Find the snapshot with the most recent modification da
