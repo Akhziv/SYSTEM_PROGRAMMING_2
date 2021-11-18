985512b2ffd3d232f163b700da0fbd4613b33cfb
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-18 05:55:05 +0000

    Add missing ChangeLog entry

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -32,4 +32,6 @@ loop do
   # Check if the date of the most recent snapshot is today
   if Time.new.to_date == File.stat(latest).mtime.to_date
     # We already have a snapshot for today. Do nothing.
-    puts "No snapshot required for #{CHA
+    puts "No snapshot required for #{CHAIN_NAME}"
+  else
+    pu
