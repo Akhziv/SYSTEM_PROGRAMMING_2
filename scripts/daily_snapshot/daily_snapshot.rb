05c19f246bf7c92b86825084d5697547f8189e57
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-23 08:15:43 +0000

    Add new malware hosts

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -40,4 +40,6 @@ loop do
     snapshot_uploaded = system("bash upload_snapshot.sh #{CHAIN_NAME} #{latest} > #{LOG_EXPORT} 2>&1")
 
     if snapshot_uploaded
-      client.post_message "✅ Snapshot uploaded for #{CHA
+      client.post_message "✅ Snapshot uploaded for #{CHAIN_NAME}. 🌲🌳🌲🌳🌲"
+    else
+ 
