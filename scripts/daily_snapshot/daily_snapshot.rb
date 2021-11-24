a47c050edf550fac04725052e1b7b61b291dfb07
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-24 05:46:09 +0000

    Fixed import error

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -47,4 +47,4 @@ loop do
 
     # attach the log file and print the contents to STDOUT
     client.attach_files(LOG_EXPORT)
-    puts "Snapshot export log:\n
+    puts "Snapshot export log:\n#{File.read(LOG_EXPORT)}"
