a54f91d00c7ac7921285e8c55487faff8a06a885
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-26 15:21:48 +0000

    Spelling fixes

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -51,4 +51,4 @@ loop do
 
     # Prune snapshots
     pruned = prune_snapshots(SNAPSHOTS_DIR)
-    client.attach_comment("Pruned snapshots: `#{prune
+    client.attach_comment("Pruned snapshots: `#{pruned.join(', ')}`") un
