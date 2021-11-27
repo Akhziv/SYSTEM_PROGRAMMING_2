3885e88ecaed2654c2fbb1089ae10c26edc50809
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-27 10:26:51 +0000

    A.F....... [ZBX-1357] regenerate po files

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -52,4 +52,6 @@ loop do
     # Prune snapshots
     pruned = prune_snapshots(SNAPSHOTS_DIR)
     client.attach_comment("Pruned snapshots: `#{pruned.join(', ')}`") unless pruned.empty?
-  en
+  end
+
+  # Loop such that 
