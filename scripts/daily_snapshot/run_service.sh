f04f1374773b5a4288f96eb590c7551851cdd4e7
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-30 17:51:40 +0000

    relocation

--- scripts/daily_snapshot/run_service.sh
+++ scripts/daily_snapshot/run_service.sh
@@ -7,4 +7,4 @@ S3_FOLDER=$BASE_FOLDER/s3
 # 1. Setup s3fs to get the snapshots.
 # 2. Make sure an instance of watchtower is running.
 # 3. Run Ruby script for exporting and uploading a new snapshot
-#    i
+#    if there isn't one for tod
