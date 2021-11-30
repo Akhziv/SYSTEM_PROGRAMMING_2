d9136e5ee2491c424db63269c6458c727de6ec9b
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-30 16:19:52 +0000

    Huh?

--- scripts/daily_snapshot/run_service.sh
+++ scripts/daily_snapshot/run_service.sh
@@ -5,4 +5,5 @@ set -e
 S3_FOLDER=$BASE_FOLDER/s3
 
 # 1. Setup s3fs to get the snapshots.
-# 2. Make sure an instance of watchtower is runn
+# 2. Make sure an instance of watchtower is running.
+# 3. Run Ruby script for 
