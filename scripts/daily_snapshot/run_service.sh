09895da835430b544f33d92c320e7a26e223d0b3
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-11-30 16:21:53 +0000

    remove key

--- scripts/daily_snapshot/run_service.sh
+++ scripts/daily_snapshot/run_service.sh
@@ -6,4 +6,4 @@ S3_FOLDER=$BASE_FOLDER/s3
 
 # 1. Setup s3fs to get the snapshots.
 # 2. Make sure an instance of watchtower is running.
-# 3. Run Ruby script for 
+# 3. Run Ruby script for exporting and upload
