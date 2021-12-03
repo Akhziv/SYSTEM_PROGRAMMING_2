0916f8d03b7e4746fda11493a25838437c56b1ce
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-12-03 19:37:13 +0000

    description updated

--- scripts/daily_snapshot/run_service.sh
+++ scripts/daily_snapshot/run_service.sh
@@ -21,3 +21,5 @@ s3fs forest-snapshots "$S3_FOLDER" \
 ## Ensure watchtower is running
 docker stop watchtower 2> /dev/null || true
 docker wait watchtower 2> /dev/null || true
+docker run --rm \
+  
