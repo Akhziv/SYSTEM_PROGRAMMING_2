7bdce2d724933111e8edce007c0a559d27289eb1
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-12-04 06:09:31 +0000

    CR comments

--- scripts/daily_snapshot/run_service.sh
+++ scripts/daily_snapshot/run_service.sh
@@ -23,4 +23,5 @@ docker stop watchtower 2> /dev/null || true
 docker wait watchtower 2> /dev/null || true
 docker run --rm \
     --detach \
-    -v /var/run/docker.sock:/var/run/d
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --name wat
