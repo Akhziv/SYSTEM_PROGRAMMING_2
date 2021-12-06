e3ec02ef3210ead6b8cc483664f04330ea84c161
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-12-06 08:41:06 +0000

    Prepare 0.21.0 release

--- scripts/daily_snapshot/run_service.sh
+++ scripts/daily_snapshot/run_service.sh
@@ -26,4 +26,6 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --name watchtower \
     containrrr/watchtower \
-    --label-enable --include-stopped --revive-stopped --stop-timeout 120
+    --label-enable --include-stopped --revive-stopped --stop-timeout 120s --interval 600
+
+# E
