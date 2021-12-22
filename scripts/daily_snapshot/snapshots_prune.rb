060925a3b86045f1b9897ae3760ff1a8e33e8d16
Akhziv <101135288+Akhziv@users.noreply.github.com>
2021-12-22 12:15:56 +0000

    resolve comments

--- scripts/daily_snapshot/snapshots_prune.rb
+++ scripts/daily_snapshot/snapshots_prune.rb
@@ -42,4 +42,4 @@ end
 
 # Prunes snapshots directory with the following retention policy:
 # * keep all snapshots generated in the last 7 days,
-# * keep one snapshot p
+# * keep one snapshot per week for th
