bb237efd2885985a404a06f0ae98ffe9d17c71c5
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-01-13 13:30:04 +0000

    Unused imports removed

--- scripts/daily_snapshot/upload_snapshot.sh
+++ scripts/daily_snapshot/upload_snapshot.sh
@@ -14,4 +14,4 @@ NEWEST_SNAPSHOT=$2
 # Make sure we have the most recent Forest image
 docker pull ghcr.io/chainsafe/forest:"${FOREST_TAG}"
 
-# Ensure that we can access file
+# Ensure that we can access files with the defaul
