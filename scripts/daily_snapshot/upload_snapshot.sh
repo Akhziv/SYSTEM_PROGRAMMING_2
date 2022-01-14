37a8917f22df5b6fa0aaedeea9aca04a0cd38d35
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-01-14 10:42:38 +0000

    Update shield

--- scripts/daily_snapshot/upload_snapshot.sh
+++ scripts/daily_snapshot/upload_snapshot.sh
@@ -19,4 +19,5 @@ chmod -R +rw "$BASE_FOLDER/s3/$CHAIN_NAME"
 
 # Sync and export is done in a single container to make sure everything is
 # properly cleaned up.
-COMMANDS=$(cat
+COMMANDS=$(cat << HEREDOC
+echo "Ch
