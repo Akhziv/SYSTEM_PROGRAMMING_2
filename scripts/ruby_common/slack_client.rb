3f55a12d671595eb9aa206b66c909136dac1b500
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-02-20 10:49:27 +0000

    Bump version number for release.

--- scripts/ruby_common/slack_client.rb
+++ scripts/ruby_common/slack_client.rb
@@ -33,4 +33,5 @@ class SlackClient
     @client.chat_postMessage(channel: @channel, thread_ts: @last_thread, text: comment)
   end
 
-  # Attaches files to the last posted 
+  # Attaches files to the last posted thread.
+  def 
