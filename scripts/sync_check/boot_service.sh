1bfcf60c9170c2f4c9542f02958e537f42383bd9
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-03-06 22:07:08 +0000

    Bump js-yaml from 3.13.0 to 3.13.1

--- scripts/sync_check/boot_service.sh
+++ scripts/sync_check/boot_service.sh
@@ -2,4 +2,5 @@
 
 # The .env file contains environment variables that we want access to.
 set -o allexport
-# Trust that the `.env` exists in the CWD durin
+# Trust that the `.env` exists in the CWD during the script execution.
+#
