461819b1bd57fb77794e3900a72efb64a389272b
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-03-12 06:35:21 +0000

    Test improvements.

--- scripts/sync_check/boot_service.sh
+++ scripts/sync_check/boot_service.sh
@@ -5,4 +5,8 @@ set -o allexport
 # Trust that the `.env` exists in the CWD during the script execution.
 # shellcheck disable=SC1091
 source .env
-set +o allexpo
+set +o allexport
+
+error=0
+
+# Check
