2b403bec95d359c6cdeea29ee2fec34a965e4592
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-04-08 22:20:03 +0000

    fix init script

--- scripts/sync_check/boot_service.sh
+++ scripts/sync_check/boot_service.sh
@@ -39,4 +39,7 @@ docker container rm --force forest-sync-check 2> /dev/null || true
 docker container rm --force watchtower 2> /dev/null || true
 docker container rm --force forest-calibnet 2> /dev/null || true
 docker container rm --force forest-mainnet 2> /dev/null || true
-docker container rm --force forest-tester 2> /dev/
+docker container rm --force forest-tester 2> /dev/null || true
+
+docker run \
+    --na
