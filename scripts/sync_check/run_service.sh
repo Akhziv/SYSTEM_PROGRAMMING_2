0e915fd684f4f46d483f8a39af1304a8513e45e4
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-05-19 13:47:20 +0000

    rtd

--- scripts/sync_check/run_service.sh
+++ scripts/sync_check/run_service.sh
@@ -5,4 +5,6 @@ set -e
 ## Ensure watchtower is running
 docker stop watchtower 2> /dev/null || true
 docker wait watchtower 2> /dev/null || true
-docker run
+docker run --rm \
+    --detach \
+    -v
