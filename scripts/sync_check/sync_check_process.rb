80769ab9ce7bf080dd68ab1beb311ee8865e939a
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-09-15 11:28:16 +0000

    Update version.txt for the branch and trunk

--- scripts/sync_check/sync_check_process.rb
+++ scripts/sync_check/sync_check_process.rb
@@ -33,4 +33,4 @@ class SyncCheck
                 ghcr.io/chainsafe/forest:#{FOREST_TAG} \
                 --config #{FOREST_SCRIPTS}/sync_check.toml \
                 #{command}")
-    raise "Failed `#{binary} #{command}`.\n```\nSTDOUT:\n#{stdout}\nS
+    raise "Failed `#{binary} #{command}`.\n```\nSTDOUT:\n#{stdout}\nSTDERR:\n#{stderr}```" unless status
