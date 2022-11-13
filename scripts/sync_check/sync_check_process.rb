3b9d8d563ff3907e619fb6ffe24efc31e1be2eb2
Akhziv <101135288+Akhziv@users.noreply.github.com>
2022-11-13 19:22:12 +0000

    Fix broken link in docs

--- scripts/sync_check/sync_check_process.rb
+++ scripts/sync_check/sync_check_process.rb
@@ -61,4 +61,4 @@ class SyncCheck
 
   # Retrieves path to the relevant snapshot based on the network chosen.
   def snapshot_path(network)
-    Dir.glob("#{FOREST_DATA}/snapshots/#{network}/*.car")[0] or raise "Can't f
+    Dir.glob("#{FOREST_DATA}/snapshots/#{network}/*.car")[0] or raise "Can't find snapshot in #{dir}"
